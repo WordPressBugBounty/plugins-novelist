7c6e5c8df3167646e736b19f63c40ab968a0ade9
Sean Murphy <sean@iamseanmurphy.com>
2025-08-02 15:17:19 +0000

    1.3.0

---
+++ novelist/vendor/composer/installed.php
@@ -0,0 +1,44 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ashleyfae/novelist',
+        'pretty_version' => '1.3.0',
+        'version' => '1.3.0.0',
+        'reference' => '2598a659bded054274150cf0aa0a83617d5c6269',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'ashleyfae/novelist' => array(
+            'pretty_version' => '1.3.0',
+            'version' => '1.3.0.0',
+            'reference' => '2598a659bded054274150cf0aa0a83617d5c6269',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v1.11.0',
+            'version' => '1.11.0.0',
+            'reference' => 'ae03311f45dfe194412081526be2e003960df74b',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+    ),
+);
